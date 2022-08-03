8ec27a0cd8b07f88a16984cb216a2b5647b3c54f
unisci <107605011+unisci@users.noreply.github.com>
2022-08-03 13:18:21 +0800

    Add files via upload

---
+++ maintbl.js
@@ -0,0 +1,10 @@
+>>>test url:/
+>>>ubr test iul=127.*:8080;
+>>>ubr test iul=255.255.255.0:<.>;
+>>>int ip;
+>>>set ip = <*>?123456789;
+>>>set def debugger uniqueID = 0972;
+>>>run . -aux -f -s -t 5 -n 10;
+>>>run bacil = ini u[data . ];
+>>>end all;
+>>>end;
