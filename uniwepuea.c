d6cbaf2927e2c605617994f747c99bd8eeb15fce
unisci <107605011+unisci@users.noreply.github.com>
2022-09-11 11:39:08 +0800

    Add files via upload

---
+++ uniwepuea.c
@@ -0,0 +1,12 @@
+>>>infc:jhu
+>>>set ih rl = .;
+>>>set tpi -.;
+>>>set uu = uuu?123456789;
+>>>when ip = N/A {
+	set ip = 127.0.0.1;
+	ping 192.168.39.2;
+	end;
+};
+>>>end process;
+>>>end all;
+>>>end;
