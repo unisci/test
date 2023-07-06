ab9458112283bf6bcde40c31e08eef4319d1fc28
unisci <107605011+unisci@users.noreply.github.com>
2023-07-06 19:16:51 +0800

    Add files via upload

---
+++ oapmu.m
@@ -0,0 +1,13 @@
+>>>set db as testdb_"模拟3";
+>>>start uslop = 0000.00000.0000.0000+0000.0000.0000.0000;
+>>>end set basea;
+>>>start aaa
+{
+	if ip = :::0
+	then set iodine= 1；
+	other0；
+}；
+end basea;
+end all;
+surf basei;
+end;
