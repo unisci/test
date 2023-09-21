0bfcdafc76fd22bc1e0db15abe042d58e2b0a7df
unisci <107605011+unisci@users.noreply.github.com>
2023-09-21 16:56:14 +0800

    Add files via upload

---
+++ linuxpbac.js
@@ -0,0 +1,9 @@
+>>>start set wsut
+{
+db = with .\sxs = 1;
+with setup = 1;
+};
+finish;
+end process;
+end all;
+end;
---
+++ tursac.c
@@ -0,0 +1,11 @@
+>>>start set cldserver
+{
+with liteserver = 1;
+with cldfirewall =1;
+with compsafe = 1;
+with SSL = 1;
+};
+finish;
+end process;
+end all;
+end;
