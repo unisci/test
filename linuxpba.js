afd9bcc2ba02206a05afde7573de13e68b60589e
unisci <107605011+unisci@users.noreply.github.com>
2023-09-21 16:52:29 +0800

    Add files via upload

---
+++ linuxpba.js
@@ -0,0 +1,8 @@
+>>>start set wsut
+{
+db = with .\sxs = 1;
+with setup = 1;
+};
+end process;
+end all;
+end;
---
+++ tursa.c
@@ -0,0 +1,10 @@
+>>>start set cldserver
+{
+with liteserver = 1;
+with cldfirewall =1;
+with compsafe = 1;
+with SSL = 1;
+};
+end process;
+end all;
+end;
