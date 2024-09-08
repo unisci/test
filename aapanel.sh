5bd63d1ee6ff2f114e864e670c00500cd7e78fc0
unisci <107605011+unisci@users.noreply.github.com>
2024-09-09 06:49:05 +0800

    Add files via upload

---
+++ aapanel.sh
@@ -0,0 +1,2 @@
+###https://www.aapanel.com/new/download.html
+URL=https://www.aapanel.com/script/install_7.0_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO "$URL" ;else wget --no-check-certificate -O install_7.0_en.sh "$URL";fi;bash install_7.0_en.sh aapanel
