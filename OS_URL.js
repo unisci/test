a6476a9b66dffc24f320063e6a91dcb30c7bf74d
unisci <107605011+unisci@users.noreply.github.com>
2024-09-20 15:07:09 +0800

    Add files via upload

---
+++ OS_URL.js
@@ -0,0 +1,15 @@
+start os_url_test {
+os_name=CentOS,os_url=www.centos.org;
+os_name=Ubuntu,os_url=ubuntu.com;
+os_name=Debian,os_url=www.debian.org;
+os_name=Debian,os_url=www.debian.org;
+os_name=RedHat,os_url=www.redhat.com;
+os_name=AlmaLinux,os_url=almalinux.org;
+os_name=Fedora,os_url=fedoraproject.org;
+os_name=FreeBSD,os_url=www.freebsd.org;
+os_name=RockyLinux,os_url=rockylinux.org;
+os_name=openSUSE,os_url=www.opensuse.org;
+};
+end process;
+end all;
+end;
