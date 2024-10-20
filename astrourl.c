62620f8f11be74c7e6982858e236e27b16d0a038
unisci <107605011+unisci@users.noreply.github.com>
2024-10-20 22:56:24 +0800

    Add files via upload

---
+++ astrourl.c
@@ -0,0 +1,186 @@
+#/*binoculars*/
+{
+[Swarovski Optik]
+company="Swarovski Optik"
+url="www.swarovskioptik.com/int/zh/outdoor/products/binoculars"
+status=ok
+};
+{
+[Leica]
+company="Leica Camera AG"
+url="leica-camera.com/en-int/sport-optics/experience-nature/binculars"
+status=ok
+url_cn="www.leica-camera.cn/about-leica-binculars"
+};
+{
+[Zeiss]
+company="Zeiss Group"
+url="www.zeiss.com/consumer-products/int/nature-observation.html#binoculars"
+status=ok
+url_cn="www.zeiss.com.cn/consumer-products/nature-observation.html#binoculars"
+};
+{
+[Canon]
+company="Canon Inc."
+url="personal.canon.jp/product/binoculars/lineup"
+status=ok
+url_cn="www.canon.com.cn/overview/telscope.html"
+};
+{
+[Nicon]
+company="Nikon Corporation"
+url="imaging.nikon.com/sport-optics/lineup/"
+status=ok
+url_cn="www.nikon.com.cn/sc_CN/products/categories/sport_optics/binoculars.page"
+}:
+#/*telescope*/
+{
+[Celestron]
+company="Celestron"
+url="www.celestron.com"
+status=ok
+url_cn="www.celestron.com.cn"
+};
+{
+[Meade]
+company="Meade Instruments"
+url="www.meade.com"
+status=ok
+};
+{
+[Sky-Watcher]
+company="Pacific Telescope Corp."
+url="www.skywatcher.com"
+status=ok
+url_cn="www.xdwyj.com"
+};
+{
+[Takahashi]
+company="株式会社 高橋製作所"
+url="takahashijapan.jp"
+status=ok
+url_cn="www.takahashicn.com"
+};
+{
+[Orion]
+company="Orion Optics"
+url="www.telescope.com"
+status=no
+};
+{
+[Vixen]
+company="株式会社 ビクセン"
+url="www.vixen.co.jp"
+status=ok
+status_cn="global.vixen.co.jp/cn/"
+};
+{
+[Sharpstar]
+company="嘉兴锐星光学仪器有限公司"
+url="gb.sharpstar-optics.com"
+status=ok
+status_en="www.sharpstar-optics.com"
+};
+{
+[WILLIAM OPTICS]
+company="WILLIAM OPTICS"
+url="williamoptics.com"
+status=ok
+};
+#/*software*/
+{
+[TheSkyX]
+company="Software Bisque"
+url="www.bisque.com"
+status=ok
+};
+{
+[StarryNight]
+company=""
+url="www.starrynight.com"
+status=ok
+};
+{
+[Stellarium]
+url="stellarium.org/zh_CN/"
+status=ok
+};
+{
+[Cartes du Ciel]
+company=""
+url=""
+status=ok
+};
+{
+[Virtual Moon Atlas]
+company=""
+url=""
+status=ok
+};
+{
+[Stellarium]
+url=""
+status=ok
+};
+#/*accessoris*/
+start process testccdurl
+{
+[ZWO]
+company="苏州振旺光电有限公司"
+url="www.zwoastro.cn"
+status=ok
+url_en="zwoasi.com"
+type=ccd
+};
+{
+[QHYCCD]
+company="光速视觉(北京)科技有限公司"
+url="www.qhyccd.cn"
+status=ok
+url_en="www.qhyccd.com"
+type=ccd
+};
+{
+[QSI]
+company="Atik Cameras Limited"
+url="qsimaging.com"
+status=ok
+type=ccd
+};
+{
+[SBIG]
+company="Diffraction Limited"
+url="diffractionlimited.com"
+status=ok
+type=ccd
+};
+{
+[Finger Lakes]
+company="Finger Lakes Instrumentation"
+url="flicamera.com"
+status=ok
+type=ccd
+};
+{
+[PlayerOne]
+company="头号玩家天文科技"
+url="player-one-astronomy.cn"
+status=ok
+url_en="player-one-astronomy.com"
+type=ccd
+};
+{
+[Tele Vue]
+company="Tele Vue Optics"
+url="www.televue.com"
+status=ok
+type=eyepieces
+};
+{
+[Pentax]
+company="リコーイメージング株式会社"
+url="www.ricoh-imaging.co.jp/japan/products/binoculars/eye/"
+status=ok
+url_cn="www.ricoh-imaging.com.cn/china/products/binoculars/eye/"
+type=eyepieces
+};
