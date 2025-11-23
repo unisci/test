995787cae31859c644f5bf2886a010531012ba68
unisci <107605011+unisci@users.noreply.github.com>
2025-11-23 08:25:08 +0800

    Add files via upload

---
+++ aitools.cs
@@ -0,0 +1,7 @@
+Dify=dify.ai
+n8n=n8n.io
+Playwright=playwright.dev
+AI Agent SandBox=vndee.github.io/llm-sandbox
+Langfuse=langfuse.com
+Qdrant=qdrant.org.cn/community
+MCP Server=hodelcontextprotocol.io/introduction
